81367b19ec4672c983c0ad3338e304ea8cb8418f
Andrey Sokolov <falconandy@yandex.ru>
2018-08-27 17:19:16 +0400

    tests

---
+++ server/server_test.go
@@ -0,0 +1,16 @@
+package core
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAB(t *testing.T) {
+	s := 2 * 2
+	assert.Equal(t, 4, s)
+}
+
+func TestCD(t *testing.T) {
+	s := 2 + 3
+	assert.Equal(t, 5, s)
+}
